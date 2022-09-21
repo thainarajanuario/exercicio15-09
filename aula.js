a4463fbd313e2aba4772812f5a2a673ac0b68dbf
thainarajanuario <gigitata199427@gmail.com>
2022-09-21 13:02:38 -0300

    Exercicios_15_09

---
+++ aula.js
@@ -0,0 +1,95 @@
+/*let n1, n2, resultado
+n1 = prompt("diegite um numero: ");
+n2 = prompt("digite o segundo numero: ");
+
+
+function abacaxi (n1, n2){
+    resultado = parseInt(n1) + parseInt(n2);
+    return alert (resultado);
+
+
+    abacaxi(n1, n2);
+}*/
+
+
+
+/*do{
+    opcao = mostrarmenu();
+}while(opcao != 2)
+
+
+function mostrarmenu()
+entrada = "";
+alert("== mostrar menu ==" +
+"/n1 - mostrar menu" +
+"/n2 - sair");
+
+return entrada = prompt();*/
+
+
+
+//exercicio1
+
+
+/*let num1, num2, porc;
+
+num1 = prompt("digite um numero: ");
+num2 = prompt("qual é a porcentagem: ");
+
+porcentagem (num1, num2)
+
+
+function porcentagem (num1, num2){
+    
+    porc = num1 *(num2/100);
+    alert ("a porcentagem é: " + porc);
+}*/
+
+
+//exercicio2
+
+/*let temperatura, celsius, fahrenheit,cels;
+
+temperatura = prompt("digite uma temperatura: ");
+
+
+     function temp (celsius){
+        fahrenheit = temperatura *(9/5) + 32;
+        alert("Sua temperatura em fahrenheit é: " + fahrenheit);
+     }temp (celsius, fahrenheit);*/
+
+
+
+
+//exercicio3
+
+/*let temperatura, celsius, fahrenheit, cels;
+
+temperatura = prompt("digite uma temperatura: ");
+
+function temp (fahrenheit){
+    celsius = temperatura * (9/5) + 32;
+    alert("Sua temperatura em celsius é: " + celsius);
+}temp (celsius, fahrenheit);*/
+
+
+//exercicio4
+
+
+/*let peso=0, Altura=0;
+
+peso = prompt("Digite seu peso: ");
+Altura = prompt("Digite sua altura: ");
+function IMC (P, A){
+    Total = P/ (A*A);
+    return(Total);
+}
+alert ( IMC(peso, Altura));*/
+
+
+
+
+
+
+
+
